fa0bb9458f22f93fbc12dd6a41d89222a6aada8b
Wang Yang <admin@wysaid.org>
2015-07-07 19:00:26 +0800

    add JNIHeader and some interfaces

---
+++ GPUImage_Android/src/main/java/org/wysaid/gpuimage_android/CGEImageHandler.java
@@ -0,0 +1,10 @@
+package org.wysaid.gpuimage_android;
+
+/**
+ * Created by wysaid on 15/7/7.
+ * Mail: admin@wysaid.org
+ * blog: wysaid.org
+ */
+public class CGEImageHandler {
+
+}
--- GPUImage_Android/src/main/java/org/wysaid/gpuimage_android/CGENativeLibrary.java
+++ GPUImage_Android/src/main/java/org/wysaid/gpuimage_android/CGENativeLibrary.java
@@ -1,5 +1,7 @@
 package org.wysaid.gpuimage_android;
 
+import android.content.Context;
+
 /**
  * Created by wysaid on 15/7/7.
  * Mail: admin@wysaid.org
@@ -13,7 +15,21 @@ public class CGENativeLibrary {
         System.loadLibrary("CGE");
     }
 
-    public static native void testFunc();
+    //全局初始化接口， 调用后将初始化并在Native层创建一个OpenGL的Context以供后面使用
+    public static boolean globalInit() {
+        return _globalInit();
+    }
+
+    //全局释放接口， 调用后将清除已创建的Context
+    public static void globalRelease() {
+        _globalRelease();
+    }
+
+    ////////////////////////////////////
 
 
+    private static native boolean _globalInit();
+    private static native void _globalRelease();
+
+//    private final Context
 }
