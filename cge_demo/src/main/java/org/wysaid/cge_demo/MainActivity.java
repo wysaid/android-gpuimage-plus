530430899b748820b1eea927b71f87c65155a376
Wang Yang <admin@wysaid.org>
2015-07-08 15:32:54 +0800

    update

--- cge_demo/src/main/java/org/wysaid/cge_demo/MainActivity.java
+++ cge_demo/src/main/java/org/wysaid/cge_demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.wysaid.gpuimage_android.CGENativeLibrary;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CGENativeLibrary.globalInit();
     }
 
     @Override
