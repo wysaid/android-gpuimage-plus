2e1fe7f4fcb9256b0d7ed28da9d3cecbccf3aa81
Wang Yang <admin@wysaid.org>
2015-07-08 19:00:53 +0800

    update

--- README.md
+++
@@ -1,4 +0,0 @@
-# android-GPUImage-plus
-GPU accelerated filters for Android based on OpenGL. 
-
-## Starting up!! ##
---
+++ library/src/main/java/org/wysaid/library/CGENativeLibrary.java
@@ -0,0 +1,32 @@
+package org.wysaid.library;
+
+/**
+ * Created by wysaid on 15/7/8.
+ * Mail: admin@wysaid.org
+ * blog: wysaid.org
+ */
+public class CGENativeLibrary {
+
+    static {
+        System.loadLibrary("gnustl_shared");
+        System.loadLibrary("CGE");
+    }
+
+    //全局初始化接口， 调用后将初始化并在Native层创建一个OpenGL的Context以供后面使用
+    public static boolean globalInit() {
+//        return cgeContextInit();
+        return false;
+    }
+
+    //全局释放接口， 调用后将清除已创建的Context
+    public static void globalRelease() {
+//        cgeContextRelease();
+    }
+
+    ////////////////////////////////////
+
+
+    private static native boolean cgeContextInit();
+    private static native void cgeContextRelease();
+
+}
