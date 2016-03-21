78545a9a7e86fb0ea2602c0f2f7fbf138cc0dbf0
Wang Yang <admin@wysaid.org>
2016-03-21 14:26:59 +0800

    新增视频录制相关功能

--- library/src/main/java/org/wysaid/nativePort/CGEFrameRecorder.java
+++ library/src/main/java/org/wysaid/nativePort/CGEFrameRecorder.java
@@ -1,7 +1,6 @@
 package org.wysaid.nativePort;
 
-//import java.nio.ByteBuffer;
-//import java.nio.ShortBuffer;
+import java.nio.ShortBuffer;
 
 /**
  * Created by wangyang on 15/7/29.
@@ -10,128 +9,112 @@ package org.wysaid.nativePort;
 //A wrapper for native class FrameRecorder
 public class CGEFrameRecorder extends CGEFrameRenderer {
 
-//    static {
-//        NativeLibraryLoader.load();
-//    }
+    static {
+        NativeLibraryLoader.load();
+    }
 
-//    public CGEFrameRecorder() {
-//        super(0); //防止多次初始化
-//        mNativeAddress = nativeCreateRecorder();
-//    }
-
-//    public void enableFaceDetectWithDefaultFilter(boolean shouldDetect) {
-//        if(shouldDetect) {
-//            nativeSetTrackedFilter(mNativeAddress, "@beautify bilateral 100 6.8 1 @adjust brightness 0.14 @adjust contrast 1.12 ");
-//        } else {
-//            nativeSetTrackedFilter(mNativeAddress, "");
-//        }
-//    }
-//
-//    public void setTrackedFilterIntensity(float intensity) {
-//        nativeSetTrackedFilterIntensity(mNativeAddress, intensity);
-//    }
-//
-//    public void setFaceArea(float x, float y, float gx, float gy) {
-//        nativeSetFaceArea(mNativeAddress, x, y, gx, gy);
-//    }
+    public CGEFrameRecorder() {
+        super(0); //防止多次初始化
+        mNativeAddress = nativeCreateRecorder();
+    }
 
     /////////////////视频录制相关////////////////////
 
-//    public boolean startRecording(int fps, String filename) {
-//        if(mNativeAddress != null)
-//            return nativeStartRecording(mNativeAddress, fps, filename);
-//        return false;
-//    }
-//
-//    public boolean isRecordingStarted() {
-//        if(mNativeAddress != null)
-//            return nativeIsRecordingStarted(mNativeAddress);
-//        return false;
-//    }
-//
-//    public boolean endRecording(boolean shouldSave) {
-//        if(mNativeAddress != null)
-//            return nativeEndRecording(mNativeAddress, shouldSave);
-//        return false;
-//    }
-//
-//    public void pauseRecording() {
-//        if(mNativeAddress != null)
-//            nativePauseRecording(mNativeAddress);
-//    }
-//
-//    public boolean isRecordingPaused() {
-//        if(mNativeAddress != null)
-//            return nativeIsRecordingPaused(mNativeAddress);
-//        return false;
-//    }
-//
-//    public boolean resumeRecording() {
-//        if(mNativeAddress != null)
-//            return nativeResumeRecording(mNativeAddress);
-//        return false;
-//    }
-//
-//    public double getTimestamp() {
-//        if(mNativeAddress != null)
-//            return nativeGetTimestamp(mNativeAddress);
-//        return 0.0;
-//    }
-//
-//    public double getVideoStreamtime() {
-//        if(mNativeAddress != null)
-//            return nativeGetVideoStreamtime(mNativeAddress);
-//        return 0.0;
-//    }
-//
-//    public double getAudioStreamtime() {
-//        if(mNativeAddress != null)
-//            return nativeGetAudioStreamtime(mNativeAddress);
-//        return 0.0;
-//    }
-//
-//    public void setTempDir(String dir) {
-//        if(mNativeAddress != null)
-//            nativeSetTempDir(mNativeAddress, dir);
-//    }
-//
-//    //需要置于GPU绘制线程
-//    public void recordImageFrame() {
-//        if(mNativeAddress != null)
-//            nativeRecordImageFrame(mNativeAddress);
-//    }
-//
-//    //需要自行loop
-//    public void recordAudioFrame(ShortBuffer audioBuffer, int bufferLen) {
-//        if(mNativeAddress != null)
-//            nativeRecordAudioFrame(mNativeAddress, audioBuffer, bufferLen);
-//    }
+    public boolean startRecording(int fps, String filename) {
+        if(mNativeAddress != 0)
+            return nativeStartRecording(mNativeAddress, fps, filename);
+        return false;
+    }
+
+    public boolean isRecordingStarted() {
+        if(mNativeAddress != 0)
+            return nativeIsRecordingStarted(mNativeAddress);
+        return false;
+    }
+
+    public boolean endRecording(boolean shouldSave) {
+        if(mNativeAddress != 0)
+            return nativeEndRecording(mNativeAddress, shouldSave);
+        return false;
+    }
+
+    public void pauseRecording() {
+        if(mNativeAddress != 0)
+            nativePauseRecording(mNativeAddress);
+    }
+
+    public boolean isRecordingPaused() {
+        if(mNativeAddress != 0)
+            return nativeIsRecordingPaused(mNativeAddress);
+        return false;
+    }
+
+    public boolean resumeRecording() {
+        if(mNativeAddress != 0)
+            return nativeResumeRecording(mNativeAddress);
+        return false;
+    }
+
+    public double getTimestamp() {
+        if(mNativeAddress != 0)
+            return nativeGetTimestamp(mNativeAddress);
+        return 0.0;
+    }
+
+    public double getVideoStreamtime() {
+        if(mNativeAddress != 0)
+            return nativeGetVideoStreamtime(mNativeAddress);
+        return 0.0;
+    }
+
+    public double getAudioStreamtime() {
+        if(mNativeAddress != 0)
+            return nativeGetAudioStreamtime(mNativeAddress);
+        return 0.0;
+    }
+
+    public void setTempDir(String dir) {
+        if(mNativeAddress != 0)
+            nativeSetTempDir(mNativeAddress, dir);
+    }
+
+    //需要置于GPU绘制线程
+    public void recordImageFrame() {
+        if(mNativeAddress != 0)
+            nativeRecordImageFrame(mNativeAddress);
+    }
+
+    //需要自行loop
+    public void recordAudioFrame(ShortBuffer audioBuffer, int bufferLen) {
+        if(mNativeAddress != 0)
+            nativeRecordAudioFrame(mNativeAddress, audioBuffer, bufferLen);
+    }
 
     /////////////////      private         ///////////////////////
 
-//    private native ByteBuffer nativeCreateRecorder();
+    private native long nativeCreateRecorder();
 
     //人脸美化相关
 
-//    private native void nativeSetTrackedFilter(ByteBuffer holder, String config);
-//    private native void nativeSetTrackedFilterIntensity(ByteBuffer holder, float intensity);
-//    private native void nativeSetFaceArea(ByteBuffer holder, float x, float y, float gx, float gy);
+//    private native void nativeSetTrackedFilter(long holder, String config);
+//    private native void nativeSetTrackedFilterIntensity(long holder, float intensity);
+//    private native void nativeSetFaceArea(long holder, float x, float y, float gx, float gy);
 
     /////////////////视频录制相关////////////////////
-//    private native boolean nativeStartRecording(ByteBuffer holder, int fps, String filename);
-//    private native boolean nativeIsRecordingStarted(ByteBuffer holder);
-//    private native boolean nativeEndRecording(ByteBuffer holder, boolean shouldSave);
-//    private native void nativePauseRecording(ByteBuffer holder);
-//    private native boolean nativeIsRecordingPaused(ByteBuffer holder);
-//    private native boolean nativeResumeRecording(ByteBuffer holder);
-//    private native double nativeGetTimestamp(ByteBuffer holder);
-//
-//    private native double nativeGetVideoStreamtime(ByteBuffer holder);
-//    private native double nativeGetAudioStreamtime(ByteBuffer holder);
-//
-//    private native void nativeSetTempDir(ByteBuffer holder, String dir);
-//
-//    private native void nativeRecordImageFrame(ByteBuffer holder);
-//    private native void nativeRecordAudioFrame(ByteBuffer holder, ShortBuffer audioBuffer, int bufferLen);
+    private native boolean nativeStartRecording(long holder, int fps, String filename);
+    private native boolean nativeIsRecordingStarted(long holder);
+    private native boolean nativeEndRecording(long holder, boolean shouldSave);
+    private native void nativePauseRecording(long holder);
+    private native boolean nativeIsRecordingPaused(long holder);
+    private native boolean nativeResumeRecording(long holder);
+    private native double nativeGetTimestamp(long holder);
+
+    private native double nativeGetVideoStreamtime(long holder);
+    private native double nativeGetAudioStreamtime(long holder);
+
+    private native void nativeSetTempDir(long holder, String dir);
+
+    private native void nativeRecordImageFrame(long holder);
+    private native void nativeRecordAudioFrame(long holder, ShortBuffer audioBuffer, int bufferLen);
 
 }
--- library/src/main/java/org/wysaid/view/CameraRecordGLSurfaceView.java
+++ library/src/main/java/org/wysaid/view/CameraRecordGLSurfaceView.java
@@ -24,5 +24,227 @@ public class CameraRecordGLSurfaceView extends CameraGLSurfaceView {
         super(context, attrs);
     }
 
-    //Not provided by now.
+    private boolean mShouldRecord = false;
+
+    public synchronized boolean isRecording() {
+        return mShouldRecord;
+    }
+
+    private final static int[] mRecordStateLock = new int[0];
+
+    private AudioRecordRunnable mAudioRecordRunnable;
+    private Thread mAudioThread;
+
+    public interface StartRecordingCallback {
+        void startRecordingOver(boolean success);
+    }
+
+    public void startRecording(final String filename){
+        startRecording(filename, null);
+    }
+
+    public synchronized void startRecording(final String filename, final StartRecordingCallback recordingCallback) {
+
+        queueEvent(new Runnable() {
+            @Override
+            public void run() {
+
+                if (mFrameRecorder == null) {
+                    Log.e(LOG_TAG, "Error: startRecording after release!!");
+                    if (recordingCallback != null) {
+                        recordingCallback.startRecordingOver(false);
+                    }
+                    return;
+                }
+
+                if (!mFrameRecorder.startRecording(30, filename)) {
+                    Log.e(LOG_TAG, "start recording failed!");
+                    if (recordingCallback != null)
+                        recordingCallback.startRecordingOver(false);
+                    return;
+                }
+                Log.i(LOG_TAG, "glSurfaceView recording, file: " + filename);
+                synchronized (mRecordStateLock) {
+                    mShouldRecord = true;
+                    mAudioRecordRunnable = new AudioRecordRunnable(recordingCallback);
+                    if (mAudioRecordRunnable.audioRecord != null) {
+                        mAudioThread = new Thread(mAudioRecordRunnable);
+                        mAudioThread.start();
+                    }
+                }
+            }
+        });
+    }
+
+    public interface EndRecordingCallback {
+        void endRecordingOK();
+    }
+
+    public void endRecording() {
+        endRecording(null);
+    }
+
+    public synchronized void endRecording(final EndRecordingCallback callback) {
+        Log.i(LOG_TAG, "notify quit...");
+        synchronized (mRecordStateLock) {
+            mShouldRecord = false;
+        }
+
+        if(mFrameRecorder == null) {
+            Log.e(LOG_TAG, "Error: endRecording after release!!");
+            return;
+        }
+
+        joinAudioRecording();
+
+        queueEvent(new Runnable() {
+            @Override
+            public void run() {
+                if (mFrameRecorder != null)
+                    mFrameRecorder.endRecording(true);
+                if (callback != null) {
+                    callback.endRecordingOK();
+                }
+            }
+        });
+    }
+
+    @Override
+    public synchronized void release(final ReleaseOKCallback callback) {
+
+        synchronized (mRecordStateLock) {
+            mShouldRecord = false;
+        }
+
+        joinAudioRecording();
+        super.release(callback);
+    }
+
+    @Override
+    public void stopPreview() {
+
+        synchronized (mRecordStateLock) {
+            if(mShouldRecord) {
+                Log.e(LOG_TAG, "The camera is recording! cannot stop!");
+                return ;
+            }
+        }
+
+        super.stopPreview();
+    }
+
+    public void joinAudioRecording() {
+
+        if(mAudioThread != null) {
+            try {
+                mAudioThread.join();
+                mAudioThread = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class AudioRecordRunnable implements Runnable {
+
+        int bufferSize;
+        short[] audioData;
+        int bufferReadResult;
+        public AudioRecord audioRecord;
+        public volatile boolean isInitialized;
+        private static final int sampleRate = 44100;
+        ShortBuffer audioBuffer;
+        StartRecordingCallback recordingCallback;
+
+        private AudioRecordRunnable(StartRecordingCallback callback)
+        {
+            recordingCallback = callback;
+            try {
+                bufferSize = AudioRecord.getMinBufferSize(sampleRate,
+                        AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate,
+                        AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,bufferSize);
+                audioData = new short[bufferSize];
+                audioBuffer = ByteBuffer.allocateDirect(bufferSize * 2).asShortBuffer();
+            } catch (Exception e) {
+                if(audioRecord != null){
+                    audioRecord.release();
+                    audioRecord = null;
+                }
+            }
+
+            if(audioRecord == null && recordingCallback != null) {
+                recordingCallback.startRecordingOver(false);
+                recordingCallback = null;
+            }
+        }
+
+        public void run() {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+            this.isInitialized = false;
+
+            if(this.audioRecord == null) {
+                recordingCallback.startRecordingOver(false);
+                recordingCallback = null;
+                return;
+            }
+
+            //判断音频录制是否被初始化
+            while (this.audioRecord.getState() == 0)
+            {
+                try
+                {
+                    Thread.sleep(100L);
+                }
+                catch (InterruptedException localInterruptedException)
+                {
+                    localInterruptedException.printStackTrace();
+                }
+            }
+            this.isInitialized = true;
+
+            try {
+                this.audioRecord.startRecording();
+            } catch (Exception e) {
+                if(recordingCallback != null) {
+                    recordingCallback.startRecordingOver(false);
+                    recordingCallback = null;
+                }
+                return;
+            }
+
+            if(this.audioRecord.getRecordingState() != AudioRecord.RECORDSTATE_RECORDING) {
+                if(recordingCallback != null) {
+                    recordingCallback.startRecordingOver(false);
+                    recordingCallback = null;
+                }
+                return;
+            }
+
+            if(recordingCallback != null) {
+                recordingCallback.startRecordingOver(true);
+                recordingCallback = null;
+            }
+
+
+            while (true) {
+                synchronized (mRecordStateLock) {
+                    if(!mShouldRecord) //&& mFrameRecorder.getVideoStreamtime() <= mFrameRecorder.getAudioStreamtime()
+                        break;
+                }
+
+                bufferReadResult = this.audioRecord.read(audioData, 0, audioData.length);
+                if (mShouldRecord && bufferReadResult > 0 && mFrameRecorder != null &&
+                        mFrameRecorder.getTimestamp() > mFrameRecorder.getAudioStreamtime()) {
+                    audioBuffer.position(0);
+                    audioBuffer.put(audioData).position(0);
+                    mFrameRecorder.recordAudioFrame(audioBuffer, bufferReadResult);
+                }
+            }
+            this.audioRecord.stop();
+            this.audioRecord.release();
+            Log.e(LOG_TAG, "音频线程退出!");
+        }
+
+    }
 }
