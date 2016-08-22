80739b1215f1ccdd32c5e7657f5165fad437176d
二狗子 <admin@wysaid.org>
2016-08-22 12:07:41 +0800

    update: fix the 'text relocation' error, decrease the library size.

--- library/src/main/java/org/wysaid/nativePort/NativeLibraryLoader.java
+++ library/src/main/java/org/wysaid/nativePort/NativeLibraryLoader.java
@@ -1,5 +1,9 @@
 package org.wysaid.nativePort;
 
+import android.util.Log;
+
+import org.wysaid.common.Common;
+
 /**
  * Created by wangyang on 15/7/30.
  */
@@ -12,11 +16,16 @@ public class NativeLibraryLoader {
             return;
         mLibraryLoaded = true;
 //        System.loadLibrary("gnustl_shared");
-        System.loadLibrary("x264.142");
-        System.loadLibrary("ffmpeg");
-        System.loadLibrary("CGE");
+//        System.loadLibrary("x264.142"); //compiled to static library in the new version.
 
-        CGEFFmpegNativeLibrary.avRegisterAll();
+//        try {
+            System.loadLibrary("ffmpeg");
+            System.loadLibrary("CGE");
+            CGEFFmpegNativeLibrary.avRegisterAll();
+//        } catch (Throwable t) {
+//            Log.e(Common.LOG_TAG, "Load library for 'cge' failed! Please check your project config.", t);
+//        }
+
     }
 
 }
