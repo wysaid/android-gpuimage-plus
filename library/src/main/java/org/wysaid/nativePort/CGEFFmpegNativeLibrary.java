7c6bf631c3b97c9b35f06766ba964bba92346f1b
Yang Wang <admin@wysaid.org>
2017-04-12 14:53:59 +0800

    some translate

--- library/src/main/java/org/wysaid/nativePort/CGEFFmpegNativeLibrary.java
+++ library/src/main/java/org/wysaid/nativePort/CGEFFmpegNativeLibrary.java
@@ -10,7 +10,8 @@ public class CGEFFmpegNativeLibrary {
         NativeLibraryLoader.load();
     }
 
-    //视频转换+特效可能执行较长的时间， 请置于后台线程运行.
+    //CN: 视频转换+特效可能执行较长的时间， 请置于后台线程运行.
+    //EN: Convert video + Filter Effects may take some time, so you'd better put it on another thread.
     public static boolean generateVideoWithFilter(String outputFilename, String inputFilename, String filterConfig, float filterIntensity, Bitmap blendImage, CGENativeLibrary.TextureBlendMode blendMode, float blendIntensity, boolean mute) {
 
         return nativeGenerateVideoWithFilter(outputFilename, inputFilename, filterConfig, filterIntensity, blendImage, blendMode == null ? 0 : blendMode.ordinal(), blendIntensity, mute);
