884520098751671247cfc8079a57623dd18956d5
Yang Wang <admin@wysaid.org>
2017-06-16 11:40:45 +0800

    add new util function

---
+++ library/src/main/java/org/wysaid/common/ConcurrentQueueHelper.java
@@ -0,0 +1,60 @@
+package org.wysaid.common;
+
+import android.util.Log;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * @Author: wangyang
+ * @Mail: admin@wysaid.org
+ * @Date: 2017/6/16
+ * @Description:
+ */
+
+public class ConcurrentQueueHelper {
+    protected ConcurrentLinkedQueue<Runnable> mQueue = new ConcurrentLinkedQueue<>();
+
+    public ConcurrentQueueHelper() {
+
+    }
+
+    public void offer(Runnable runnable) {
+        mQueue.offer(runnable);
+    }
+
+    public void consume() {
+
+        try {
+
+            Runnable runnable = null;
+
+            do {
+                runnable = mQueue.poll();
+                if(runnable != null)
+                    runnable.run();
+            } while (runnable != null);
+        } catch (Throwable throwable) {
+            Log.e(Common.LOG_TAG, "ConcurrentQueueHelper: " + throwable.getLocalizedMessage());
+        }
+    }
+
+    public boolean isEmpty() {
+        return mQueue.isEmpty();
+    }
+
+    public void consumeLast() {
+
+        try {
+            Runnable runnable = null;
+
+            do {
+                runnable = mQueue.poll();
+                if(runnable != null)
+                    runnable.run();
+            } while (runnable != null);
+        } catch (Throwable throwable) {
+            Log.e(Common.LOG_TAG, "ConcurrentQueueHelper: " + throwable.getLocalizedMessage());
+        }
+    }
+
+}
