6606bd6a2ac3cbdf45424b912fcf2587e85a0db1
Niek Akerboom <niek.akerboom@marinosoftware.com>
2017-09-27 17:59:39 +0200

    pre-recorded videos will now end up with correct rotation when using the CGEFFmpegNativeLibrary.generateVideoWithFilter(...) method

--- library/src/main/jni/interface/cgeVideoDecoder.h
+++ library/src/main/jni/interface/cgeVideoDecoder.h
@@ -257,6 +257,8 @@ namespace CGE
 
 		AVDictionary* getOptions();
 
+		const char *getRotate();
+
 	protected:
 		CGEVideoDecodeContext* m_context;
 		int m_width, m_height;
@@ -267,7 +269,9 @@ namespace CGE
 		double m_currentTimestamp;
 
 	private:
-		unsigned char* m_bufferPtr; //视频帧缓冲
+		unsigned char* m_bufferPtr; //视频帧缓冲\
+
+		const char* extractMetadataInternal(const char *key);
 	};
 }
 
