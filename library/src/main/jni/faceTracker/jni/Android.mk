be67a1590e19defe11cb9d7806340a54f1499030
Yang Wang <admin@wysaid.org>
2018-01-06 14:52:19 +0800

    fix compile issue of `opnecv` not found #269

--- library/src/main/jni/faceTracker/jni/Android.mk
+++ library/src/main/jni/faceTracker/jni/Android.mk
@@ -6,17 +6,6 @@
 
 MAKEFILE_PATH := $(call my-dir)
 
-###############################
-
-#include $(CLEAR_VARS)
-
-#OPENCV_INSTALL_MODULES:=on
-#OPENCV_LIB_TYPE:=STATIC
-
-#CV_ANDROID=/Users/wysaid/android_develop/opencv-android-sdk3.1
-
-#include $(CV_ANDROID)/sdk/native/jni/OpenCV.mk
-
 ##############################
 
 OPENCV_ROOT=$(MAKEFILE_PATH)/../../opencv
@@ -42,73 +31,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := tbb
 LOCAL_SRC_FILES := $(CVLIB_DIR)/libtbb.a
 include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_calib3d
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_calib3d.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_features2d
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_features2d.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_flann
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_flann.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_imgcodecs
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_imgcodecs.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_ml
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_ml.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_photo
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_photo.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_shape
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_shape.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_stitching
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_stitching.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_superres
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_superres.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_video
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_video.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_videoio
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_videoio.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := opencv_videostab
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libopencv_videostab.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := IlmImf
-# LOCAL_SRC_FILES := $(CVLIB_DIR)/libIlmImf.a
-# include $(PREBUILT_STATIC_LIBRARY)
-
-###############################
 
 
 include $(CLEAR_VARS)
@@ -149,21 +71,6 @@ LOCAL_STATIC_LIBRARIES := \
 			opencv_core \
 			tbb 
 
-# opencv_imgcodecs
-# opencv_calib3d \
-# opencv_features2d \
-# opencv_flann \
-# opencv_imgcodecs \
-# opencv_ml \
-# opencv_photo \
-# opencv_shape \
-# opencv_stitching \
-# opencv_superres \
-# opencv_video \
-# opencv_videoio \
-# opencv_videostab \
-# IlmImf
-
 LOCAL_CPPFLAGS := -frtti -std=gnu++11
 LOCAL_LDLIBS :=  -llog -ljnigraphics -lz -ldl -lm -latomic
 
@@ -178,7 +85,7 @@ ifeq ($(TARGET_ARCH_ABI),armeabi)
 LOCAL_CFLAGS := $(LOCAL_CFLAGS) -mfloat-abi=softfp
 endif
 
-LOCAL_SHARED_LIBRARIES := opencv CGE
+LOCAL_SHARED_LIBRARIES := CGE
 
 LOCAL_ARM_MODE := arm
 
