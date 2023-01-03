933f907eb4a3e1eb5e442395ac2884e57db54e42
wysaid <this@wysaid.org>
2023-01-04 03:10:18 +0800

    finish work of: https://github.com/wysaid/android-gpuimage-plus/pull/510

--- library/src/main/jni/cge/filters/cgeWaveformFilter.cpp
+++ library/src/main/jni/cge/filters/cgeWaveformFilter.cpp
@@ -2,7 +2,7 @@
 
 #include <EGL/egl.h>
 
-static CGEConstString s_vshWaveform = "#version 310 es\n" CGE_SHADER_STRING_PRECISION_H(
+static CGEConstString s_vshWaveform = "#version 320 es\n" CGE_SHADER_STRING_PRECISION_H(
     layout(location = 0) in vec2 position;
     layout(location = 0) out vec2 textureCoordinate;
     void main() {
@@ -10,18 +10,23 @@ static CGEConstString s_vshWaveform = "#version 310 es\n" CGE_SHADER_STRING_PREC
         textureCoordinate = (position.xy + 1.0) / 2.0;
     });
 
-static CGEConstString s_fshWaveform = "#version 310 es\n" CGE_SHADER_STRING(
+static CGEConstString s_fshWaveform = "#version 320 es\n" CGE_SHADER_STRING(
     precision highp float;
     precision highp int;
     layout(location = 0) in vec2 textureCoordinate;
     layout(binding = 0) uniform sampler2D inputImageTexture;
-    layout(rgba8, binding = 1) uniform writeonly image2D outputImageTexture;
+    layout(rgba8ui, binding = 1) uniform writeonly highp uimage2D outputImage;
     layout(location = 0) out vec4 fragColor;
 
     void main() {
-        fragColor = texture(inputImageTexture, textureCoordinate);
+        fragColor = vec4(1.0);
+        vec4 color = texture(inputImageTexture, textureCoordinate);
+        float lum = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+        ivec2 newLoc = ivec2(vec2(textureCoordinate.x, lum) * vec2(imageSize(outputImage)));
+        uint newLum = uint(lum * 255.0);
+        imageStore(outputImage, newLoc, uvec4(newLum, newLum, newLum, 255));
 
-        fragColor.r = 1.0;
+        // TODO: 考虑使用 imageAtomicAdd 保障原子操作 (不闪屏)
     });
 
 namespace CGE
@@ -34,10 +39,12 @@ bool CGEWaveformFilter::init()
 {
     if (initShadersFromString(s_vshWaveform, s_fshWaveform))
     {
-        setFormPosition(0.5f, 0.5f);
-        setFormSize(0.4f, 0.3f);
+        m_program.bind();
+        setFormPosition(0.1f, 0.1f);
+        setFormSize(0.3f, 0.3f);
         setColor(0.0f, 0.0f, 0.0f, 0.5f);
         m_drawer.reset(TextureDrawer::create());
+        m_drawer->setFlipScale(1.0f, -1.0f); // flip upside down, meet the gl coord.
         return true;
     }
 
@@ -53,20 +60,31 @@ bool CGEWaveformFilter::init()
 void CGEWaveformFilter::render2Texture(CGEImageHandlerInterface* handler, GLuint srcTexture, GLuint vertexBufferID)
 {
     handler->setAsTarget();
+    glClear(GL_COLOR_BUFFER_BIT);
     m_program.bind();
 
     /// 渲染不写入, 使用 imageStore 写入.
-    // glColorMask(false, false, false, false);
+    glColorMask(false, false, false, false);
 
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glActiveTexture(GL_TEXTURE0);
-    // glBindImageTexture(0, srcTexture, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA8);
     glBindTexture(GL_TEXTURE_2D, srcTexture);
-    // glActiveTexture(GL_TEXTURE1);
-    // glBindImageTexture(1, handler->getTargetTextureID(), 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA8);
-    // glBindTexture(GL_TEXTURE_2D, handler->getTargetTextureID());
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindImageTexture(1, handler->getTargetTextureID(), 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA8UI);
+
     glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    glColorMask(true, true, true, true);
+    glMemoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT);
+
+    handler->swapBufferFBO();
+    handler->setAsTarget();
+    auto&& sz = handler->getOutputFBOSize();
+    glViewport(m_position[0] * sz.width, m_position[1] * sz.height, m_size[0] * sz.width, m_size[1] * sz.height);
+    m_drawer->drawTexture(handler->getBufferTextureID());
 }
 
 void CGEWaveformFilter::setFormPosition(float left, float top)
