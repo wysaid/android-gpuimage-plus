5c0cf5a02b5ec48a3b9681de283bfc99862809d2
xiaodi86 <51728141+xiaodi86@users.noreply.github.com>
2023-03-01 09:50:32 +0800

    删除背景设设置setColor
    删除背景设设置setColor

--- library/src/main/jni/cge/filters/cgeHistogramFilter.h
+++ library/src/main/jni/cge/filters/cgeHistogramFilter.h
@@ -4,38 +4,26 @@
 #include "cgeImageFilter.h"
 #include "cgeTextureUtils.h"
 #include "cgeVec.h"
-namespace CGE
-{
-	class CGEHistogramFilter : public CGEImageFilterInterface
-	{
-	public:
-		~CGEHistogramFilter() override;
 
-		/**
-         * @brief 左上角的点
-         */
-		void setFormPosition(float left, float top);
-		/**
-         * @brief 相对大小
-         */
-		void setFormSize(float width, float height);
+namespace CGE{
+class CGEHistogramFilter : public CGEImageFilterInterface
+    {
+    public:
+        ~CGEHistogramFilter() override;
 
-		/**
-         * @brief 背景色
-         */
-		void setColor(float r, float g, float b, float a);
+        void setFormPosition(float left, float top);
 
-		bool init() override;
+        void setFormSize(float width, float height);
 
-		void render2Texture(CGEImageHandlerInterface* handler, GLuint srcTexture, GLuint vertexBufferID) override;
+        bool init() override;
 
-	protected:
-		std::unique_ptr<TextureDrawer> m_drawer;
-		std::unique_ptr<FrameBufferWithTexture> m_renderTarget;
-		Vec2f m_position;
-		Vec2f m_size;
-		Vec4f m_color;
-	};
-} // namespace CGE
+        void render2Texture(CGEImageHandlerInterface* handler, GLuint srcTexture, GLuint vertexBufferID) override;
 
+    protected:
+        std::unique_ptr<TextureDrawer> m_drawer;
+        std::unique_ptr<FrameBufferTexture> m_renderTarget;
+        Vec2f m_position;
+        Vec2f m_size;
+    };
+}
 #endif
