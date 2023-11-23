c49fb7039a919d24c6d79d510455860871d1b548
wy <this@wysaid.org>
2023-11-23 20:35:22 +0800

    support mingw build on Windows

--- utils/platform_utils.sh
+++ utils/platform_utils.sh
@@ -37,12 +37,39 @@ function isLinux() {
     [[ "$(uname -s)" == "Linux" ]]
 }
 
+if isMingW; then
+    echo "Detected MingW, fix cmd.exe path..." >&2
+    # Bug issue: <https://stackoverflow.com/questions/11865085/out-of-a-git-console-how-do-i-execute-a-batch-file-and-then-return-to-git-conso>
+    # Tested on Git Bash - Git for Windows v2.42.0(2)
+    # Do not work: cmd /C $@
+    # Do not work too: cmd "/C $@"
+    # Workaround below:
+    function cmd() {
+        echo "mingw call cmd start: 'cmd.exe $(echo $@)'" >&2
+        cmd.exe "$(echo $@)"
+        echo "mingw call cmd end: 'cmd.exe $(echo $@)'" >&2
+    }
+
+elif isWindows && ! command -v cmd &>/dev/null; then
+    echo "cmd not found, detect windows native cmd.exe"
+    if [[ -f "/mnt/c/Windows/system32/cmd.exe" ]]; then
+        function cmd() {
+            /mnt/c/Windows/system32/cmd.exe $@
+        }
+    elif [[ -f "/cygdrive/c/Windows/system32/cmd.exe" ]]; then
+        # Not tested
+        function cmd() {
+            /cygdrive/c/Windows/system32/cmd.exe $@
+        }
+    fi
+fi
+
 function getEnvironmentVariable() {
-    VAR_NAME="$@"
+    echo "getEnvironmentVariable $@" >&2
+    VAR_NAME=$@
     RET_VALUE=
     if isWindows && command -v cmd &>/dev/null; then
-        # Detect native var first
-        RET_VALUE=$(cmd /C set "$VAR_NAME" | sed 's/.*=//')
+        RET_VALUE=$(cmd /C set $VAR_NAME | sed 's/.*=//')
     fi
 
     if [[ -z "$RET_VALUE" ]]; then
@@ -54,35 +81,21 @@ function getEnvironmentVariable() {
     fi
 }
 
-if isWindows && ! command -v cmd &>/dev/null; then
-    if [[ -f "/mnt/c/Windows/system32/cmd.exe" ]]; then
-        function cmd() {
-            /mnt/c/Windows/system32/cmd.exe $@
-        }
-    elif [[ -f "/c/Windows/system32/cmd.exe" ]]; then
-        function cmd() {
-            /c/Windows/system32/cmd.exe $@
-        }
-    elif [[ -f "/cygdrive/c/Windows/system32/cmd.exe" ]]; then
-        function cmd() {
-            /cygdrive/c/Windows/system32/cmd.exe $@
-        }
-    fi
-fi
-
 function runGradleCommand() {
 
     if isWindows && [[ -f "$REPO_DIR/local.properties" ]] && [[ -n "$(grep -i "sdk.dir=" "$REPO_DIR/local.properties" | grep -E ':(\\|/)')" ]]; then
         # Run with Windows Native
         echo "Perform Windows Native Gradle Func: "
-        cmd /C gradlew $@ && exit 0
+        cmd /C gradlew.bat $@ && exit 0
+
         # Go ahead if failed.
         echo "Perform Windows Native Gradle Failed, Go with current shell env"
         export GRADLE_RUN_WIN_FAILED=true
     fi
 
+    echo "Perform Gradle Func: "
     if ! ./gradlew $@ && [[ -z "$GRADLE_RUN_WIN_FAILED" ]]; then
         # Try again with Windows Native
-        command -v cmd &>/dev/null && cmd /C gradlew $@
+        command -v cmd &>/dev/null && cmd /C gradlew.bat $@
     fi
 }
